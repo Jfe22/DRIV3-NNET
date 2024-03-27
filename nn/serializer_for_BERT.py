9eb292e7b323c7f2cc5b6a26863c0ab50dc3787a
Jfe22 <2181706@my.ipleiria.pt>
2024-03-27 16:07:16 +0000

    model training on data

--- nn/serializer_for_BERT.py
+++ nn/serializer_for_BERT.py
@@ -24,29 +24,23 @@ tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 train_encodings = tokenizer(train_texts, truncation=True, padding=True)
 eval_encodings = tokenizer(eval_texts, truncation=True, padding=True)
 
-# Convert training labels to ints because BERT expects labels to be integers
-temp_train_list = train_labels
-label_dict = {}
-int_labels = []
-for i in range(len(temp_train_list)):
-    label_dict[i] = temp_train_list[i]
-    int_labels.append(i)
 
-print(temp_train_list)
+# Create a mapping from label classes to integers
+label_map = {"Aggressive": 0, "Slow": 1, "Normal": 2}
 
+# Convert label strings to integers based on the mapping
+train_labels_int = [label_map[label] for label in train_labels]
+eval_labels_int = [label_map[label] for label in eval_labels]
 
 # Create TensorDatasets
 train_dataset = TensorDataset(
-    torch.tensor(train_encodings['input_ids']),
-    torch.tensor(train_encodings['attention_mask']),
-    #torch.tensor(train_labels)
-    torch.tensor(int_labels)
-    
-)
-eval_dataset = TensorDataset(
-    torch.tensor(eval_encodings['input_ids']),
-    torch.tensor(eval_encodings['attention_mask']),
-    #torch.tensor(eval_labels)
-    torch.tensor(int_labels)
+    torch.tensor(train_encodings['input_ids'], dtype=torch.long),  
+    torch.tensor(train_encodings['attention_mask'], dtype=torch.long),  
+    torch.tensor(train_labels_int, dtype=torch.long)  
 )
 
+eval_dataset = TensorDataset(
+    torch.tensor(eval_encodings['input_ids'], dtype=torch.long),  
+    torch.tensor(eval_encodings['attention_mask'], dtype=torch.long),  
+    torch.tensor(eval_labels_int, dtype=torch.long) 
+)
