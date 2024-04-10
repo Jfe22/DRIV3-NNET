88f0e6717131a6b052f2a25d1e13df0780159c1e
Jfe22 <2181706@my.ipleiria.pt>
2024-04-10 15:35:41 +0100

    pandas code now concatenates all aggresive, normal and slow datasets

--- dataset/dataset_generator.py
+++ dataset/dataset_generator.py
@@ -1,18 +1,12 @@
 import pandas as pd
 
-DEBUG = 0
-
-concatenated_df = pd.DataFrame()
+DEBUG = 1
 concatenated_rows = []
+window_size = 4
+increment = 1
 
-# Read aggressive datasets
-for i in range(1, 11):
-  df = pd.read_csv(f"../dataset/datasets_for_pandas/dataset_agg{i}.csv")
-  if (DEBUG): print(f'dataframe: {df}')
-
-  window_size = 4
-  increment = 1
-  for i in range(len(df) - window_size + 1):
+def concatenate_dataset(df, window_size, increment, concatenated_rows):
+  for i in range(len(df) - window_size + increment):
     window = df.iloc[i:i + window_size]
     if (DEBUG): print("window: " + window) 
     
@@ -20,8 +14,21 @@ for i in range(1, 11):
     label = window.iloc[0]['label'] 
 
     concatenated_rows.append({'sensor_data': concatenated_sensor_data, 'label': label})
-    if (not DEBUG): print("sensor data: " + concatenated_sensor_data) 
-    if (not DEBUG): print(f'concated rows: {concatenated_rows}') 
+    if (DEBUG): print("sensor data: " + concatenated_sensor_data) 
+    if (DEBUG): print(f'concated rows: {concatenated_rows}')
+
+for i in range(1, 11):
+  df = pd.read_csv(f"datasets_for_pandas/dataset_agg{i}.csv")
+  if (DEBUG): print(f'dataframe agg{i}: {df}')
+  concatenate_dataset(df, window_size, increment, concatenated_rows)
+
+  df = pd.read_csv(f"datasets_for_pandas/dataset_normal{i}.csv")
+  if (DEBUG): print(f'dataframe normal{i}: {df}')
+  concatenate_dataset(df, window_size, increment, concatenated_rows)
+
+  df = pd.read_csv(f"datasets_for_pandas/dataset_slow{i}.csv")
+  if (DEBUG): print(f'dataframe slow{i}: {df}')
+  concatenate_dataset(df, window_size, increment, concatenated_rows)
 
 concatenated_df = pd.DataFrame(concatenated_rows) 
-concatenated_df.to_csv('test_dataset1.csv', index=False)
+concatenated_df.to_csv('datasets_for_training/1sec_timeframe_dataset.csv', index=False)
