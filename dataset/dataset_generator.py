e2220a512c690b929613b55e60288e3157a5d87b
Jfe22 <2181706@my.ipleiria.pt>
2024-04-13 20:19:35 +0100

    1_sec_timeframe_dataset is now fixed and has only acc and gyro values

--- dataset/dataset_generator.py
+++ dataset/dataset_generator.py
@@ -13,16 +13,6 @@ def parse_sensor_data(window):
   gyroY = []
   gyroZ = []
 
-
-
-  if (DEBUG): print(window)
-  if (DEBUG): print(window['sensor_data'])
-  if (DEBUG): print(window['sensor_data'][0])
-  if (DEBUG): print(window['sensor_data'][1])
-  if (DEBUG): print(window['sensor_data'][2])
-  if (DEBUG): print(window['sensor_data'][3])
-  if (DEBUG): print(len(window['sensor_data']))
-
   for i in range(len(window['sensor_data'])):
     row = window['sensor_data'][i].split("/")
     accX.append(float(row[0].replace('$$$', '')))
@@ -32,39 +22,17 @@ def parse_sensor_data(window):
     gyroY.append(float(row[4]))
     gyroZ.append(float(row[5]))
 
-    print(row)
-
-
   return accX, accY, accZ, gyroX, gyroY, gyroZ
 
 
-  #testing = ' '.join(map(str, window['sensor_data']))
-  #if (not DEBUG): print(testing)
-  return
-
 def set_label(window):
   accX, accY, accZ, gyroX, gyroY, gyroZ = parse_sensor_data(window)
-  # check data variance and apply label
-  print(accX)
-  print(accY)
-  print(accZ)
-  print(gyroX)
-  print(gyroY)
-  print(gyroZ)
-
   accXVariance = max(accX) - min(accX) 
   accYVariance = max(accY) - min(accY)
   accZVariance = max(accZ) - min(accZ)
   gyroXVariance = max(gyroX) - min(gyroX)
   gyroYVariance = max(gyroY) - min(gyroY)
   gyroZVariance = max(gyroZ) - min(gyroZ)
-
-  print(accXVariance)
-  print(accYVariance)
-  print(accZVariance)
-  print(gyroXVariance)
-  print(gyroYVariance)
-  print(gyroZVariance)
 
   weight_accX = 1/6
   weight_accY = 1/6
@@ -74,9 +42,12 @@ def set_label(window):
   weight_gyroZ = 1/6
 
   driving_variance = (accXVariance * weight_accX) + (accYVariance * weight_accY) + (accZVariance * weight_accZ) + (gyroXVariance * weight_gyroX) + (gyroYVariance * weight_gyroY) + (gyroZVariance * weight_gyroZ)
-  print(driving_variance)
-  # e por fim, decidir qual as balizas para valores de variancia que indicam uma conducao perigosa
-  return 
+
+  if (driving_variance < 0.3): label = 'Slow'
+  if (driving_variance >= 0.3 and driving_variance < 0.6): label = 'Normal'
+  if (driving_variance >= 0.6): label = 'Aggresive'
+
+  return label 
 
 def concatenate_dataset(df, window_size, increment, concatenated_rows):
   for i in range(len(df) - window_size + increment):
@@ -88,13 +59,6 @@ def concatenate_dataset(df, window_size, increment, concatenated_rows):
     #uncoment and replace the above line when function is ready 
     #label = set_label(window)
 
-    ##### TEST BED #####
-    if (not DEBUG): set_label(window)
-    exit()
-
-
-    ####################
-
     concatenated_rows.append({'sensor_data': concatenated_sensor_data, 'label': label})
     if (DEBUG): print("sensor data: " + concatenated_sensor_data) 
     if (DEBUG): print(f'concated rows: {concatenated_rows}')
@@ -103,8 +67,6 @@ for i in range(1, 11):
   df = pd.read_csv(f"datasets_for_pandas/dataset_agg{i}.csv")
   if (DEBUG): print(f'dataframe agg{i}: {df}')
   concatenate_dataset(df, window_size, increment, concatenated_rows)
-  #only for testing
-  exit()
 
   df = pd.read_csv(f"datasets_for_pandas/dataset_normal{i}.csv")
   if (DEBUG): print(f'dataframe normal{i}: {df}')
@@ -114,7 +76,6 @@ for i in range(1, 11):
   if (DEBUG): print(f'dataframe slow{i}: {df}')
   concatenate_dataset(df, window_size, increment, concatenated_rows)
 
-exit()
 concatenated_df = pd.DataFrame(concatenated_rows) 
-#concatenated_df.to_csv('datasets_for_training/1sec_timeframe_dataset.csv', index=False)
-concatenated_df.to_csv('datasets_for_training/auto_label_test1.csv', index=False)
+concatenated_df.to_csv('datasets_for_training/1sec_timeframe_dataset.csv', index=False)
+#concatenated_df.to_csv('datasets_for_training/auto_label_test1.csv', index=False)
