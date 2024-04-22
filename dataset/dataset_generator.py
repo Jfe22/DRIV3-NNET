39e7637c670378bc97c8cfd00129e317d8f4c3b6
Jfe22 <2181706@my.ipleiria.pt>
2024-04-23 00:30:17 +0100

    data augmentation - 2x more data now

--- dataset/dataset_generator.py
+++ dataset/dataset_generator.py
@@ -54,44 +54,21 @@ def create_window(accX, accY, accZ, gyroX, gyroY, gyroZ):
   return concatenated_data
 
 def extra_data(window):
-    accX, accY, accZ, gyroX, gyroY, gyroZ = parse_sensor_data(window)
-    random_value = random.uniform(-0.01, 0.01)
-    newAccX = [x + random_value for x in accX]
-    newAccY = [x + random_value for x in accY]
-    newAccZ = [x + random_value for x in accZ]
-    newGyroX = [x + random_value for x in gyroX]
-    newGyroY = [x + random_value for x in gyroY]
-    newGyroZ = [x + random_value for x in gyroZ]
+  accX, accY, accZ, gyroX, gyroY, gyroZ = parse_sensor_data(window)
+  random_value = random.uniform(-0.01, 0.01)
+  newAccX = [x + random_value for x in accX]
+  newAccY = [x + random_value for x in accY]
+  newAccZ = [x + random_value for x in accZ]
+  newGyroX = [x + random_value for x in gyroX]
+  newGyroY = [x + random_value for x in gyroY]
+  newGyroZ = [x + random_value for x in gyroZ]
 
-    print(f'accX: {accX}')
-    print(f'accY: {accY}')
-    print(f'accZ: {accZ}')
-    print(f'gyroX: {gyroX}')
-    print(f'gyroY: {gyroY}')
-    print(f'gyroZ: {gyroZ}')
+  concatenated_data = create_window(newAccX, newAccY, newAccZ, newGyroX, newGyroY, newGyroZ)
+  column_name = "sensor_data"
+  concatenated_data_final = pd.DataFrame({column_name: concatenated_data})
+  label = set_label(concatenated_data_final['sensor_data']) 
 
-    print(f'newAccX: {newAccX}')
-    print(f'newAccY: {newAccY}')
-    print(f'newAccZ: {newAccZ}')
-    print(f'newGyroX: {newGyroX}')
-    print(f'newGyroY: {newGyroY}')
-    print(f'newGyroZ: {newGyroZ}')
-
-    concatenated_data = create_window(newAccX, newAccY, newAccZ, newGyroX, newGyroY, newGyroZ)
-    column_name = "sensor_data"
-    concatenated_data_final = pd.DataFrame({column_name: concatenated_data})
-    label = set_label(concatenated_data_final['sensor_data']) 
-
-    return concatenated_data_final, label
-
-    # ja temos os novos valores de cada sensor
-    # podemos retornar los e criar a linhas noutra func ou entao
-    # criar a linha aqui mesmo e retornar pronto a concatenar c o resto, idk  
-
-    
-
-
-
+  return concatenated_data_final, label
 
 def concatenate_dataset(df, window_size, increment, concatenated_rows):
   for i in range(len(df) - window_size + increment):
@@ -99,30 +76,23 @@ def concatenate_dataset(df, window_size, increment, concatenated_rows):
     if (DEBUG): print("window: " + window) 
     
     concatenated_sensor_data = ' '.join(map(str, window['sensor_data']))
-    #label = window.iloc[0]['label'] 
     label = set_label(window['sensor_data'])
 
     concatenated_rows.append({'sensor_data': concatenated_sensor_data, 'label': label})
     if (DEBUG): print("sensor data: " + concatenated_sensor_data) 
-    if (DEBUG): print(f'concated rows: {concatenated_rows}')
+    if (not DEBUG): print(f'concated rows: {concatenated_rows}')
 
-    #we will do the dataaugmentation here
-    dataaug1_concatenated_sensor_data, dataaug1_label = extra_data(window['sensor_data'])
-    
+    #data augmentation - x2
+    dataaug1_sensor_data, dataaug1_label = extra_data(window['sensor_data'])
+    dataaug1_concatenated_sensor_data = ' '.join(map(str, dataaug1_sensor_data['sensor_data']))
 
-  
-  
+    concatenated_rows.append({'sensor_data': dataaug1_concatenated_sensor_data, 'label': dataaug1_label})
+    if (not DEBUG): print(f'concated rows: {concatenated_rows}')
 
 for i in range(1, 11):
   df = pd.read_csv(f"datasets_for_pandas/dataset_agg{i}.csv")
   if (DEBUG): print(f'dataframe agg{i}: {df}')
-
   concatenate_dataset(df, window_size, increment, concatenated_rows)
-  exit()
-  #aqui arranjar maneira de chamar o concatenate 3 vezes
-  #a primeira para a tabela normal que originaria a single row
-  #e a segunda e terceira parra as novas linhas/tabelas criadas a partir da og
-  #ou seja data augmentation
 
   df = pd.read_csv(f"datasets_for_pandas/dataset_normal{i}.csv")
   if (DEBUG): print(f'dataframe normal{i}: {df}')
@@ -132,7 +102,5 @@ for i in range(1, 11):
   if (DEBUG): print(f'dataframe slow{i}: {df}')
   concatenate_dataset(df, window_size, increment, concatenated_rows)
 
-exit()
 concatenated_df = pd.DataFrame(concatenated_rows) 
-#concatenated_df.to_csv('datasets_for_training/1sec_timeframe_dataset.csv', index=False)
-concatenated_df.to_csv('datasets_for_training/auto_label_test1.csv', index=False)
+concatenated_df.to_csv('datasets_for_training/auto_label_data_augmented_1.csv', index=False)
--- nn/__main__.py
+++ nn/__main__.py
@@ -1,8 +1,8 @@
 from model_train import train_driving
 from model_eval import eval_driving
 
-dataset = "auto_label_test1.csv"
-model_name = "BERT_AUTO_LABEL_TRAIN_NOT_TEST_3"
+dataset = "auto_label_data_augmented_1.csv"
+model_name = "BERT_AUTO_LABEL_TRAIN_NOT_TEST_WITH_DATA_AUG"
 
 train_driving(dataset, model_name, 30)
 eval_driving(dataset, model_name) 
