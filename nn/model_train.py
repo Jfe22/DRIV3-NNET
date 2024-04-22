f4f1ddb51f2655e1ded4372de1821c8af0536af8
Jfe22 <2181706@my.ipleiria.pt>
2024-04-22 16:45:52 +0100

    training and eval data separated

--- nn/model_train.py
+++ nn/model_train.py
@@ -28,6 +28,8 @@ class CustomDataCollator(DataCollatorWithPadding):
     
 def train_driving(dataset, new_model_name, epochs):
   train_dataset, eval_dataset = get_tensors('../dataset/datasets_for_training/' + dataset)
+  exit()
+
   tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
   model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=3)
 
--- nn/serializer_for_BERT.py
+++ nn/serializer_for_BERT.py
@@ -2,31 +2,34 @@ import pandas as pd
 import torch
 from transformers import BertTokenizer
 from torch.utils.data import TensorDataset
+from sklearn.model_selection import train_test_split
 
 def get_tensors(dataset_path):
-    train_df = pd.read_csv(dataset_path)
-    eval_df = pd.read_csv(dataset_path)
-    train_texts = train_df['sensor_data'].tolist()
-    eval_texts = eval_df['sensor_data'].tolist()
-    train_labels = train_df['label'].tolist()
-    eval_labels = eval_df['label'].tolist()
+  train_df = pd.read_csv(dataset_path)
+  texts = train_df['sensor_data'].tolist()
+  labels = train_df['label'].tolist()
 
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    train_encodings = tokenizer(train_texts, truncation=True, padding=True)
-    eval_encodings = tokenizer(eval_texts, truncation=True, padding=True)
+  train_texts, train_labels, eval_texts, eval_labels = train_test_split(texts, labels, test_size=0.1, random_state=42)
 
-    label_map = {"Aggressive": 0, "Slow": 1, "Normal": 2}
-    train_labels_int = [label_map[label] for label in train_labels]
-    eval_labels_int = [label_map[label] for label in eval_labels]
+  tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+  train_encodings = tokenizer(train_texts, truncation=True, padding=True)
+  eval_encodings = tokenizer(eval_texts, truncation=True, padding=True)
 
-    train_dataset = TensorDataset(
-      torch.tensor(train_encodings['input_ids'], dtype=torch.long),  
-      torch.tensor(train_encodings['attention_mask'], dtype=torch.long),  
-      torch.tensor(train_labels_int, dtype=torch.long)  
-    )
-    eval_dataset = TensorDataset(
-      torch.tensor(eval_encodings['input_ids'], dtype=torch.long),  
-      torch.tensor(eval_encodings['attention_mask'], dtype=torch.long),  
-      torch.tensor(eval_labels_int, dtype=torch.long) 
-    )
-    return train_dataset, eval_dataset
+  label_map = {"Aggressive": 0, "Slow": 1, "Normal": 2}
+  train_labels_int = [label_map[label] for label in train_labels]
+  eval_labels_int = [label_map[label] for label in eval_labels]
+
+  train_dataset = TensorDataset(
+    torch.tensor(train_encodings['input_ids'], dtype=torch.long),  
+    torch.tensor(train_encodings['attention_mask'], dtype=torch.long),  
+    torch.tensor(train_labels_int, dtype=torch.long)  
+  )
+  eval_dataset = TensorDataset(
+    torch.tensor(eval_encodings['input_ids'], dtype=torch.long),  
+    torch.tensor(eval_encodings['attention_mask'], dtype=torch.long),  
+    torch.tensor(eval_labels_int, dtype=torch.long) 
+  )
+
+  print ("Train dataset size:", len(train_dataset))
+  print ("Eval dataset size:", len(eval_dataset))
+  return train_dataset, eval_dataset
