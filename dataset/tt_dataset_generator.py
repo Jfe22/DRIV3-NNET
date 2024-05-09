b4614890aa004127ccdb1415b86602133765e7a9
Jfe22 <2181706@my.ipleiria.pt>
2024-05-09 17:39:24 +0100

    datasets with non norm sensor value sum as attributes, final results normalized and non normalized

--- dataset/tt_dataset_generator.py
+++ dataset/tt_dataset_generator.py
@@ -52,25 +52,39 @@ def set_label_sum(accX, accY, accZ):
   if (sumAcc > 4.3): return "Aggressive"
   return "Normal"
 
-def get_windows(data):
+def get_windows(data, window_type):
   concatenated_4instarray_dataset = pd.DataFrame() 
   for i in range(len(data) - window_size + increment):
     window = data[i:i+window_size]
     accX, accY, accZ, gyroX, gyroY, gyroZ, label = get_arrays(window)
     label = set_label_sum(accX, accY, accZ)
 
-    new_row = pd.DataFrame({'accelerometerXAxis': [accX], 
-                            'accelerometerYAxis': [accY],  
-                            'accelerometerZAxis': [accZ],
-                            'gyroscopeXAxis': [gyroX], 
-                            'gyroscopeYAxis': [gyroY], 
-                            'gyroscopeZAxis': [gyroZ], 
-                            'label': [label]}, index=[0])
+    if window_type == 'array':
+      new_row = pd.DataFrame({'accelerometerXAxis': [accX], 
+                              'accelerometerYAxis': [accY],  
+                              'accelerometerZAxis': [accZ],
+                              'gyroscopeXAxis': [gyroX], 
+                              'gyroscopeYAxis': [gyroY], 
+                              'gyroscopeZAxis': [gyroZ], 
+                              'label': [label]}, index=[0])
+    elif window_type == 'sum':
+      new_row = pd.DataFrame({'accelerometerXAxis': [sum(accX)], 
+                              'accelerometerYAxis': [sum(accY)],  
+                              'accelerometerZAxis': [sum(accZ)],
+                              'gyroscopeXAxis': [sum(gyroX)], 
+                              'gyroscopeYAxis': [sum(gyroY)], 
+                              'gyroscopeZAxis': [sum(gyroZ)], 
+                              'label': [label]}, index=[0])
+    else:
+      print("Invalid window type")
+      return
+    
     concatenated_4instarray_dataset = pd.concat([concatenated_4instarray_dataset, new_row], ignore_index=True)
 
     if DEBUG: print(concatenated_4instarray_dataset)
     if DEBUG: print(type(concatenated_4instarray_dataset))
   return concatenated_4instarray_dataset 
+
 
 def read_normalized_data():
   for i in range(1, 11):
@@ -103,24 +117,47 @@ def read_non_normalized_data():
   dataframe = pd.concat(concatenated_raw_dataset)
   return dataframe 
 
-def create_sum_labeled_dataset():
+def create_sum_labeled_dataset(att_type):
   concatenated_raw_dataset = read_non_normalized_data()
-  windowed_raw_dataset = get_windows(concatenated_raw_dataset)
+  if att_type == "sum": windowed_raw_dataset = get_windows(concatenated_raw_dataset, "sum")
+  if att_type == "array": windowed_raw_dataset = get_windows(concatenated_raw_dataset, "array")
   return windowed_raw_dataset
 
-def normalize_dataset(non_norm_data):
-  norm_data = get_windows(read_normalized_data())
+def normalize_array_dataset(non_norm_data):
+  norm_data = get_windows(read_normalized_data(), "array")
   norm_data['label'] = non_norm_data['label']
   return norm_data
 
-non_norm_dataframe = create_sum_labeled_dataset()
-norm_final_dataframe = normalize_dataset(non_norm_dataframe)
-norm_final_dataframe.to_csv('datasets_for_training/tt_final_labels_from_accsum_post_norm.csv', index=False)
+
+# label: based on the sum of the accelerometer values
+# attributes: arrays of size 4 (4 instances of time) for each sensor
+def create_sum_label_array_att_dataset():
+  non_norm_dataframe = create_sum_labeled_dataset("array")
+  norm_final_dataframe = normalize_array_dataset(non_norm_dataframe)
+  norm_final_dataframe.to_csv('datasets_for_training/tt_final_labels_from_accsum_post_norm.csv', index=False)
+  # GENERATE THESE DATASETS TO COMPARE WITH THE FINAL DATASET
+  #norm_without_c_labels = get_windows(read_normalized_data())
+  #not_norm_with_c_labels = get_windows(read_non_normalized_data())
+  #norm_without_c_labels.to_csv('datasets_for_training/tt_final_norm_without_c_labels.csv', index=False)
+  #not_norm_with_c_labels.to_csv('datasets_for_training/tt_final_not_norm_with_c_labels.csv', index=False)
 
 
-# GENERATE THESE DATASETS TO COMPARE WITH THE FINAL DATASET
-#norm_without_c_labels = get_windows(read_normalized_data())
-#not_norm_with_c_labels = get_windows(read_non_normalized_data())
-#norm_without_c_labels.to_csv('datasets_for_training/tt_final_norm_without_c_labels.csv', index=False)
-#not_norm_with_c_labels.to_csv('datasets_for_training/tt_final_not_norm_with_c_labels.csv', index=False)
+# label: based on the sum of the accelerometer values
+# attributes: sum of non-normalized sensor data for the 4 instances of time
+def create_sum_label_sum_att_dataset():
+  non_norm_dataframe = create_sum_labeled_dataset("sum")
+  non_norm_dataframe.to_csv('datasets_for_training/tt_final_labels_from_accsum_with_att_accsum.csv', index=False)
 
+# label: based on the sum of the accelerometer values
+# attributes: sum of non-normalized sensor data for the 4 instances of time (normalized post sum)
+def create_sum_label_sum_att_normalized_dataset():
+  non_norm_dataframe = create_sum_labeled_dataset("sum")
+  non_label_columns = [col for col in non_norm_dataframe.columns if col != 'label']
+  norm_dataframe = non_norm_dataframe.copy()
+  # Min max normalization
+  norm_dataframe[non_label_columns] = (norm_dataframe[non_label_columns] - norm_dataframe[non_label_columns].min()) / (norm_dataframe[non_label_columns].max() - norm_dataframe[non_label_columns].min())
+  norm_dataframe.to_csv('datasets_for_training/tt_final_labels_from_accsum_with_att_accsum_normalized.csv', index=False)
+
+#create_sum_label_array_att_dataset()
+#create_sum_label_sum_att_dataset()
+create_sum_label_sum_att_normalized_dataset()
